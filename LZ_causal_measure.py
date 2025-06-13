a5a9dbce7eb976381f86d45027386f195739151e
frogloki <dhruthiravikiran@gmail.com>
2025-06-13 11:35:37 +0530

    Refined method and added comments

--- LZ_causal_measure.py
+++ LZ_causal_measure.py
@@ -1,79 +1,54 @@
 import numpy as np
 
-
 def LZ_penalty(sequence_gr, sequence_cmp):
+    #Initialize Grammar Sets G_x and G_y
     sub_strings_gr = set()
     sub_strings_cmp = set()
-    k_cap = set()
+    #Initialize SubString pointers
     ind_gr = 0
     inc_gr = 1
     ind_cmp = 0
     inc_cmp = 1
-    #find grammar of x
+    #Initialize overlap extent to 0
+    overlap = 0
+    #Main loop
     while True:
-        #if dictionary of x has to be built
-        if ind_gr + inc_gr <= len(sequence_gr):
+        #Find a substring not in G_x
+        while True:
+            if ind_gr + inc_gr > len(sequence_gr):
+                break
             sub_str_gr = sequence_gr[ind_gr : ind_gr + inc_gr]
             if sub_str_gr in sub_strings_gr:
                 inc_gr += 1
             else:
-                sub_strings_gr.add(sub_str_gr)
-                ind_gr += inc_gr
-                inc_gr = 1
-
-                while True:
-                    sub_str_cmp = sequence_cmp[ind_cmp : ind_cmp + inc_cmp]
-                    if ind_cmp + inc_cmp > len(sequence_cmp):
-                        break
-                    if sub_str_cmp in sub_strings_gr and sub_str_cmp not in sub_strings_cmp:
-                        sub_strings_cmp.add(sub_str_cmp)
-                        k_cap.add(sub_str_cmp)
-                        ind_cmp += inc_cmp
-                        inc_cmp = 1
-                        break
-                    if sub_str_cmp in sub_strings_cmp:
-                        inc_cmp += 1
-                    else:
-                        sub_strings_cmp.add(sub_str_cmp)
-                        ind_cmp += inc_cmp
-                        inc_cmp = 1
-                        break
-                    # print(sub_strings_gr)
-                    # print(sub_strings_cmp)
-                    # print(k_cap)
-                    # print('\nmeow')
- 
-                if ind_cmp + inc_cmp > len(sequence_cmp):
-                        break
-                
-        else:
-            while True:
-                sub_str_cmp = sequence_cmp[ind_cmp : ind_cmp+inc_cmp]
-                if ind_cmp + inc_cmp > len(sequence_cmp):
-                    break
-                if sub_str_cmp in sub_strings_gr and sub_str_cmp not in sub_strings_cmp:
-                    sub_strings_cmp.add(sub_str_cmp)
-                    k_cap.add(sub_str_cmp)
-                    ind_cmp += inc_cmp
-                    inc_cmp = 1
-                if sub_str_cmp in sub_strings_cmp:
-                    inc_cmp += 1
-                else:
-                    sub_strings_cmp.add(sub_str_cmp)
-                    ind_cmp += inc_cmp
-                    inc_cmp = 1
-                # print(sub_strings_gr)
-                # print(sub_strings_cmp)
-                # print(k_cap)
-                # print('\nhi')
-            if ind_cmp + inc_cmp > len(sequence_cmp):
                 break
-    # print(sub_strings_gr)
-    # print(sub_strings_cmp)
-    # print(k_cap)
-    # print('\nhi')
+        #Add it to G_x if such a substring exists. Move pointers to next part of the string.
+        if ind_gr + inc_gr <= len(sequence_gr):
+            sub_strings_gr.add(sub_str_gr)
+            ind_gr += inc_gr
+            inc_gr = 1
+        
+        #Do the same for sequence y.
+        while True:
+            if ind_cmp + inc_cmp > len(sequence_cmp): 
+                break
+            sub_str_cmp = sequence_cmp[ind_cmp : ind_cmp + inc_cmp]
+            if sub_str_cmp in sub_strings_cmp:
+                inc_cmp += 1
+            else: 
+                break
+        if ind_cmp + inc_cmp > len(sequence_cmp):
+            break
+        sub_strings_cmp.add(sub_str_cmp)
+        #If this substring is already present in G_x, increase overlap by one.
+        if sub_str_cmp in sub_strings_gr:
+            overlap += 1
+        ind_cmp += inc_cmp
+        inc_cmp = 1
 
-    return len(sub_strings_cmp) - len(k_cap)
+        # print(sub_strings_gr, sub_strings_cmp)
+    return len(sub_strings_cmp) - overlap
    
 def calc_penalty(x,y):
     return LZ_penalty(x,y), LZ_penalty(y,x)
+print(LZ_penalty("100","100"))
